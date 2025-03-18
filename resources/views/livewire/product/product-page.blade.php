925282c197bda65739b3e31094829304cd30a3d1
brian <liar88828@gmail.com>
2025-03-18 18:22:37 +0700

    save fix style

--- resources/views/livewire/product/product-page.blade.php
+++ resources/views/livewire/product/product-page.blade.php
@@ -1,16 +1,18 @@
 <div class='flex gap-2 sm:gap-4 flex-col'>
-    <div class='flex justify-between'>
+    <div class='flex justify-between items-end'>
         {{--        <form class="join" wire:submit='render'>--}}
-        <input type="search" class="input " placeholder="Search..." wire:model.live.debounce.1000ms="search"/>
+        <input type="search" class="input sm:input-md input-sm"
+               placeholder="Search..."
+               wire:model.live.debounce.1000ms="search"/>
         {{--            <span wire:text="search"></span>--}}
         {{--            <button type='submit' class='btn join-item'>Search</button>--}}
         {{--        </form>--}}
 
-        <a href='{{ route('product.create') }}' class='btn btn-info btn-sm'>Create</a>
+        <a href='{{ route('product.create') }}' class='btn btn-info btn-sm sm:input-md'>Create</a>
     </div>
 
     <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-        <table class="table">
+        <table class="table-xs sm:table-sm md:table">
             <thead>
             <tr>
                 <th>ID</th>
@@ -20,8 +22,6 @@
                 <th>Action</th>
             </tr>
             </thead>
-
-
             <tbody>
             <span wire:loading>Saving...</span>
             @foreach ($products as $product)
@@ -30,12 +30,12 @@
                     <td>{{$product->name}}</td>
                     <td>{{$product->qty}}</td>
                     <td>{{$product->desc}}</td>
-                    <td class="flex flex-wrap gap-1">
+                    <td class="flex flex-wrap gap-1 justify-center md:justify-start">
                         <a href="{{route('product.update',$product->id)}}"
-                           class='btn btn-sm btn-warning'>Update</a>
+                           class='btn btn-xs sm:btn-sm btn-warning'>Update</a>
                         <button wire:confirm="Are you sure you want to delete this post?"
                                 wire:click="onDelete({{$product->id}})"
-                                type="button" class='btn btn-sm btn-error'>
+                                type="button" class='btn btn-xs sm:btn-sm btn-error'>
                             Delete
                         </button>
 
@@ -46,10 +46,8 @@
             <tfoot>
             <tr>
                 <td colspan="5">
-
                     {{ $products->links() }}
                 </td>
-
             </tr>
             </tfoot>
         </table>
