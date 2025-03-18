dfd50972931fe655a50640ba3da9561d033b2368
brian <liar88828@gmail.com>
2025-03-18 16:08:08 +0700

    save counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class='card'>
+    <div class='card-body'>
+        <h1 class='card-title'>{{ $count }}</h1>
+        <button wire:click="increment" class='btn '>+</button>
+        <button wire:click='decrement'class='btn '>-</button>
+    </div>
 </div>
+
